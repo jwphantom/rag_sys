bbc9fca0d2f749cfc6a1cef0e8903f7d4af6f6c8
jwphantom <44881349+jwphantom@users.noreply.github.com>
2024-08-17 11:46:30 +0100

    fix bug

--- app/utils/complex_input.py
+++ app/utils/complex_input.py
@@ -1,7 +1,4 @@
-from langchain_community.vectorstores import Chroma
 from app.utils.setupRAG import (
-    create_retriever,
-    load_and_split_document,
     setup_embedding_and_llm,
     create_hybrid_retriever,
 )
@@ -28,13 +25,14 @@ def generate_prompt(input, pdf_path, history, canal):
             Votre nom : Skylia
             Utilisez le contexte suivant (délimité par <ctx></ctx>) et l'historique du chat (délimité par <hs></hs>) pour répondre à la question :\n
             Instructions sur la façon de répondre : \n
-            - Toujours accompagné tes bonjours de ton nom
+            - Si dans l'historique <hs></hs> il n'y a qu'un seul échange (celui de User :), commence ta réponses par une salutation suivi de ton nom 
+            - Si dans l'historique <hs></hs> tu as déjà salué, ne le fait plus après
             - Si la phrase est une salutation suivie d'une question, recherche la question adéquate.  
             - Si la question est une salutation suivie d'une forme de demande, recherche la réponse adéquate et contextuelle.  
             - Si la question est une forme de demande, recherche la réponse adéquate et contextuelle.   
             - Pour les questions dont la réponse est explicite dans le passage, répondez intégralement sans reformuler.
             - Pour les questions sans réponse explicite dans le passage, répondez en combinant les informations recueillies dans le document.
-            - la question : {question} peut être une combinaisonde plusieurs question, bien vouloir répondre à toutes les questions du moment ou elle sont dans le contexte
+            - la question : {request} peut être une combinaison de plusieurs question, bien vouloir répondre à toutes les questions du moment ou elle sont dans le contexte
             - Les phrases doivent être cohérentes et sans fautes.
             - Les réponses doivent être rédigées uniquement en FRANÇAIS (C'est une injonction).
             - N'utilisez pas de phrases telles que « D'après le document », « D'après le passage », « Les informations fournies », « Dans le texte », ou toute autre réponse qui implique que vous générez à partir d'un extrait donné. Répondez plutôt comme si vous étiez un être humain ayant acquis ces connaissances dès la naissance.     
@@ -42,7 +40,9 @@ def generate_prompt(input, pdf_path, history, canal):
             - Si vous avez du mal à trouver le contexte, ne dites pas « D'après le document », « D'après le passage », « Les informations fournies », « Dans le texte », ou toute autre chose qui montre que vous avez utilisé un passage ou un document. Dites simplement : Je suis désolé mais cela va au-delà de mes capacités.            
             - Utilisez si ça existe l'historique du chat entre <hs></hs> pour assurer un suivi cohérent de la conversation sinon base toi uniquement sur la question.
             - Utilisez l'historique pour vous souvenir de l'échange
+            - Si tu juges que la requête ente <rq></rq> est une fin de conversation comme ["Merci pour les informations","Merci", "A plus tard", "Okay", etc], genre toute forme de remerciement et message de stop de conversation retourne juste ENDCONV
             - Format de la réponse : Juste la réponse, des symboles, des caractères ou du texte et pas de résumé, juste et strictement la réponse. 
+            - Ne pas employer le pronomn personnel au singulier, toujours te considérer comme un (nous) vu que tu réponds au nom d'une entreprise
         ------
         <ctx>
         {context}
@@ -52,9 +52,9 @@ def generate_prompt(input, pdf_path, history, canal):
         {history}
         </hs>
         ------
-        <qt>
-        {question}
-        </qt>
+        <rq>
+        {request}
+        </rq>
         Réponse :
         """
 
@@ -66,12 +66,12 @@ def generate_prompt(input, pdf_path, history, canal):
         """
 
     prompt = PromptTemplate(
-        input_variables=["history", "context", "question"],
+        input_variables=["history", "context", "request"],
         template=template,
     )
 
     # Générer le prompt formaté
-    formatted_prompt = prompt.format(history=history, context=context, question=input)
+    formatted_prompt = prompt.format(history=history, context=context, request=input)
 
     responses = llm.invoke(formatted_prompt)
 
