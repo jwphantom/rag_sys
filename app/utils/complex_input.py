667d26c1e89e1dbf09ac6df91bce3bf0c4f1625a
jwphantom <jawill.olongo@gmail.com>
2024-11-01 01:37:57 +0100

    fix bug

--- app/utils/complex_input.py
+++ app/utils/complex_input.py
@@ -1,66 +1,78 @@
-from langchain_community.embeddings import GPT4AllEmbeddings
+from app.utils.setupRAG import (
+    setup_embedding_and_llm,
+    create_hybrid_retriever,
+)
 
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain.prompts import PromptTemplate
 
+import logging
 
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_groq import ChatGroq
-import os
-
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-
-from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
-from langchain.schema import Document
-from langchain_community.vectorstores import FAISS
+# Configuration du logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-# Setup for embedding and LLM
-def setup_embedding_and_llm():
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.2)
-    embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    return llm, embedding
+def generate_prompt(input, pdf_path, history, canal):
 
+    llm, _ = setup_embedding_and_llm()
 
-# Load and split document text
-def load_and_split_document(pdf_path):
-    loader = PyMuPDFLoader(pdf_path)
-    data = loader.load()
-    chunk_size = 1000
-    chunk_overlap = 100
+    retriever = create_hybrid_retriever(pdf_path)
+    docs = retriever.invoke(input)
+    context = "\n".join([doc.page_content for doc in docs])
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    # Template pour le prompt,
+    template = """
+            Tu t'appele désormais Skylia
+            Utilisez le contexte suivant (délimité par <ctx></ctx>) et l'historique du chat (délimité par <hs></hs>) pour répondre à la question :\n
+            Instructions sur la façon de répondre : \n
+            - Si dans l'historique <hs></hs> il n'y a qu'un seul échange (celui de User :), commence ta réponses par une salutation suivi de ton nom genre Bonjour je m'appelle Skylia...
+            - Si dans l'historique <hs></hs> tu as déjà salué, ne le fait plus après
+            - Si la phrase est une salutation suivie d'une question, recherche la question adéquate.  
+            - Si la question est une salutation suivie d'une forme de demande, recherche la réponse adéquate et contextuelle.  
+            - Si la question est une forme de demande, recherche la réponse adéquate et contextuelle.   
+            - Pour les questions dont la réponse est explicite dans le passage, répondez intégralement sans reformuler.
+            - Pour les questions sans réponse explicite dans le passage, répondez en combinant les informations recueillies dans le document.
+            - la question : {request} peut être une combinaison de plusieurs question, bien vouloir répondre à toutes les questions du moment ou elle sont dans le contexte
+            - Les phrases doivent être cohérentes et sans fautes.
+            - Les réponses doivent être rédigées uniquement en FRANÇAIS (C'est une injonction).
+            - N'utilisez pas de phrases telles que « D'après le document », « D'après le passage », « Les informations fournies », « Dans le texte », ou toute autre réponse qui implique que vous générez à partir d'un extrait donné. Répondez plutôt comme si vous étiez un être humain ayant acquis ces connaissances dès la naissance.     
+            - Si la question est hors contexte ou hors de propos, ou si vous ne trouvez pas le passage pour y répondre, dites simplement : Je suis désolé mais cela va au-delà de mes capacités           
+            - Si vous avez du mal à trouver le contexte, ne dites pas « D'après le document », « D'après le passage », « Les informations fournies », « Dans le texte », ou toute autre chose qui montre que vous avez utilisé un passage ou un document. Dites simplement : Je suis désolé mais cela va au-delà de mes capacités.            
+            - Utilisez si ça existe l'historique du chat entre <hs></hs> pour assurer un suivi cohérent de la conversation sinon base toi uniquement sur la question.
+            - Utilisez l'historique pour vous souvenir de l'échange
+            - Si tu juges que la requête ente <rq></rq> est une fin de conversation comme ["Merci pour les informations","Merci", "A plus tard", "Okay", etc], genre toute forme de remerciement et message de stop de conversation retourne juste ENDCONV
+            - Format de la réponse : Juste la réponse, des symboles, des caractères ou du texte et pas de résumé, juste et strictement la réponse. 
+            - Ne pas employer le pronomn personnel au singulier, toujours te considérer comme un (nous) vu que tu réponds au nom d'une entreprise
+        ------
+        <ctx>
+        {context}
+        </ctx>
+        ------
+        <hs>
+        {history}
+        </hs>
+        ------
+        <rq>
+        {request}
+        </rq>
+        Réponse :
+        """
+
+    if canal == "email":
+        template += """
+        Réponses améliorées
+        Essaie de comprendre le texte dans le contenu historique entre <hs></hs> qui provient des conversations par courriel de l'utilisateur afin de répondre de manière cohérente.
+        
+        """
+
+    prompt = PromptTemplate(
+        input_variables=["history", "context", "request"],
+        template=template,
     )
-    all_splits = text_splitter.split_documents(data)
 
-    return all_splits
+    # Générer le prompt formaté
+    formatted_prompt = prompt.format(history=history, context=context, request=input)
 
+    responses = llm.invoke(formatted_prompt)
 
-# Fonction pour configurer le retriever
-def create_retriever(pdf_path):
-    _, embedding = setup_embedding_and_llm()
-    all_splits = load_and_split_document(pdf_path)
-    vectorstore = Chroma.from_documents(documents=all_splits, embedding=embedding)
-    retriever = vectorstore.as_retriever()
-    return retriever
-
-
-def create_hybrid_retriever(pdf_path):
-    _, embedding = setup_embedding_and_llm()
-    all_splits = load_and_split_document(pdf_path)
-    docs = [
-        Document(page_content=split.page_content, metadata=split.metadata)
-        for split in all_splits
-    ]
-
-    bm25_retriever = BM25Retriever.from_documents(docs)
-    faiss_vectorstore = FAISS.from_documents(docs, embedding)
-    faiss_retriever = faiss_vectorstore.as_retriever()
-
-    ensemble_retriever = EnsembleRetriever(
-        retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5]
-    )
-    return ensemble_retriever
+    return responses.content
